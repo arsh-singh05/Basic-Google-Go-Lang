064895a22bca2fc4adc0e7d89d41abe658d1ac75
Arshdeep Singh <81232152+arsh-singh05@users.noreply.github.com>
2023-01-21 20:18:28 +0400

    Create ArrayBasic.go

---
+++ Array/ArrayBasic.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() 
+{
+    var a [5]int
+    a[0] = 1
+    a[1] = 2
+    a[2] = 3
+    a[3] = 4
+    a[4] = 5
+    fmt.Println(a)
+}    
+
+// This program declares an array `a` of length 5 and assigns values to each element. 
+// Then it uses the `fmt` package to print the entire array to the console.
