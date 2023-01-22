3e269f350c337b0c7f475980f5fed62348e5599a
Arshdeep Singh <81232152+arsh-singh05@users.noreply.github.com>
2023-01-22 22:23:35 +0400

    Create BasicIfElse.go

---
+++ Conditional Statements/BasicIfElse.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() 
+{
+    x := 5
+    if x > 0 
+  {
+        fmt.Println("x is positive")
+   }
+  else 
+  {
+        fmt.Println("x is non-positive")
+    }
+}
